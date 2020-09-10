64d731d7c6c2da1fcf796acdb4ba3e18eb2c400e
leandroturrion <leandrourielturrion@hotmail.com>
2020-09-10 18:35:17 -0300

    actualize las vistas de login y  register

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const controller = require('../controllers/userControllers')
 
-router.get('/registarse', controller.register);
+router.get('/registrarse', controller.register);
 router.get('/iniciarsesion', controller.iniciarsesion);
 
 module.exports = router;
