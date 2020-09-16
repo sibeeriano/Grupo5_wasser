1bad286b287006de9862d44ee8e159601b469df9
LeandroBucco <leandrobucco@gmail.com>
2020-09-16 17:25:06 -0300

    agregando cosas

--- validators/registerValidator.js
+++ validators/registerValidator.js
@@ -47,5 +47,8 @@ module.exports = [
          return true
      })
        
-     .withMessage("Las contraseñas no coinciden")
+     .withMessage("Las contraseñas no coinciden"),
+     check('bases')
+     .isString('on')
+     .withMessage('Debe aceptar las bases y condiciones')
     ]
