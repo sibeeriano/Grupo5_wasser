dbe20d84df768d02eb2a548cae31d5b67e42622a
Uriel Turrion <leandrourielturrion@hotmail.com>
2020-10-26 15:33:03 -0300

    login terminado, faltar user y addprod

--- public/javascript/userLogin.js
+++ public/javascript/userLogin.js
@@ -1,42 +1,37 @@
-window.onload = function(){
-    
+window.addEventListener('load',function(){
     let loginForm = document.getElementById('loginForm')
     let inputEmail = document.getElementById('email')
     let inputPass = document.getElementById('password')
-    //let inputRecordar = document.getElementById('exampleCheck1')
-    let divErrors = document.getElementById('errors')
-    let regexEmail =  /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/
-    
-    //console.log(inputRecordar) chequea la vinculación
+    let regexEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
 
-    function loginValidator(){
+
+
+    function loginValitor(){
         let errores = []
-        //el email tenga un formato tipo email (regex email validator en google)
-        if(regexEmail.test(inputEmail.value)){
-            errores.push('El email es incorrecto')
-        }
-        //que la contraseña tenga como minimo 6 y maximo 12 igual que en el back
-        //if(!inputPass.value.lenght < 6 || inputPass.value.lenght > 12){ //si el lenght de la pss es menor a 6 o mayor a 12 entra al error
-        //errores.push('La contraseña tiene que tener entre 6 y 12 caracteres')
-        //}
 
-        return errores //esta funcion devuelve los errores si los hay
-    }
- 
-    loginForm.submit = (e)=>{//con esto quer emos evitar que se de la accion por defecto que es que se envie el formulario de una, para evitar esto le damos un parametro llamado "e"
-        e.preventDefault() //con esto no se envia el formulario, no pasa nada
-        
-        divErrors.innerHTML ="" //con esto no se acumulan los errores en la vista!
-
-        let errores = loginValidator()  
-    
-        if (errores.lenght==0){  //si errores es igual a 0 
-            loginForm.submit() //se envia el formulario 
-        }else {//sino pio primero creo un div en el form para que se muestren los errores con la siguiente logica:
-            errores.forEach(error => {
-                divErrors.innerHTML += `<div class="alert alert-danger" role ="alert"> ${error}</div>`
-            })
+        if(!regexEmail.test(inputEmail.value)){
+            errores.push('Debes ingresar un email valido')
         }
-        
+        if(!inputPass.value.length < 0){
+            errores.push('Debes ingresar una contraseña')
+        }
+
+        return errores
+
     }
-}
+
+    loginForm.onsubmit = (e) =>{
+        e.preventDefault()
+
+        let errores = loginValitor()
+
+        if(errores.length > 0){
+
+        }else{
+            loginForm.submit()
+        }
+
+    }
+
+
+})
--- public/javascript/userRegister.js
+++ public/javascript/userRegister.js
@@ -1,28 +1,14 @@
-/*
-los id del formulario son 
-
-formulario---> id='formRegister'
-nombre-------> id='nombre'
-apellido-----> id='apellido'
-email--------> id='email'
-paswsword1---> id='password'
-password2----> id='password2'
-imagen-------> id='customFileLang'
-
-ya esta el <div> de los errores en la vista
-
-el script esta en /views/partials/scripts como "  <script src="/javascript/userRegister.js"></script>  "
-
-*/
-
+window.onload = function(){
 let formRegister = document.getElementById('formRegister') //todos los elementos capturados
+console.log(formRegister)
 let inputNombre = document.getElementById('nombre')
 let inputApellido = document.getElementById('apellido')
 let inputEmail = document.getElementById('email')
 let inputPass = document.getElementById('password')
 let inputPass2 = document.getElementById('password2')
 let inputImagen = document.getElementById('customFileLang')
+let errors = document.getElementById('errors')
 
-let divErrors = document.getElementById('errors')
 
-//agregar logica para cada elemento
+
+}
